612d3ac8210ea6ab029564a88a03e41e62fe64a7
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-18 13:10:19 +0100

    load project

---
+++ unit_tests/raw_test/Cart.test.js
@@ -0,0 +1,17 @@
+const Cart = require("./Cart.js");
+
+const cart = new Cart();
+cart.addToCart("cheesecake");
+
+const hasOneItem = cart.items.length === 1;
+const hasAcheesecake = cart.items[0] === "cheesecake";
+
+if(hasOneItem && hasAcheesecake) {
+    console.log("The addToCart function can add an item to the cart");
+} else {
+    const actualContent = cart.items.join(", ");
+    console.error("The addToCart function didn't do what we expect!");
+    console.error(`Here is the actual content of the cart: ${actualContent}`);
+
+    throw new Error("Thest faild!");
+}
