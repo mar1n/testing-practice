d051bba0f5efc8b09f44278f6fcb9c3f4d1cf85a
mar1n <30738216+mar1n@users.noreply.github.com>
2021-04-18 14:00:25 +0100

    multiple tests with jest

---
+++ unit_tests/jest_multiple_tests/Cart.test.js
@@ -0,0 +1,15 @@
+const Cart = require("./Cart.js");
+
+test("The addToCart function can add an item to the cart", () => {
+    const cart = new Cart();
+    cart.addToCart("cheesecake");
+    expect(cart.items).toEqual(["cheesecake"]);
+});
+
+test("The removeFromCart function can remove an item from the cart", () => {
+    const cart = new Cart();
+    cart.addToCart("cheesecake");
+    cart.removeFromCart("cheesecake");
+
+    expect(cart.items).toEqual([]);
+})
