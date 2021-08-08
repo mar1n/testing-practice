1909f8b2d6248a8cdd49b83542b8ed6b6b49a7b0
mar1n <30738216+mar1n@users.noreply.github.com>
2021-08-08 19:19:29 +0100

    breaking down test to small parts

---
+++ organising_test_suit/server.test.js
@@ -0,0 +1,40 @@
+const { app, inventory, carts } = require("./server");
+const fetch = require("isomorphic-fetch");
+
+const apiRoot = "http://localhost:3000";
+
+const addItem = (username, item) => {
+    return fetch(`${apiRoot}/carts/${username}/items/${item}`, {
+        method: "POST"
+    });
+};
+
+describe("addItem", () => {
+    beforeEach(() => carts.clear());
+    beforeEach(() => inventory.set("cheesecake", 1));
+
+    test("correct response", async () => {
+        const addItemResponse = await addItem("lucas", "cheesecake");
+        expect(addItemResponse.status).toBe(200);
+        expect(await addItemResponse.json()).toEqual(["cheesecake"]);
+    });
+
+    test("inventory update", async () => {
+        await addItem("lucas", "cheesecake");
+        expect(inventory.get("cheesecake")).toBe(0);
+    });
+
+    test("cart update", async() => {
+        await addItem("keith", "cheesecake");
+        expect(carts.get("keith")).toEqual(["cheesecake"]);
+    });
+
+    test("soldout items", async() => {
+        inventory.set("cheesecake", 0);
+        const failedAddItem = await addItem("lucas", "cheesecake");
+        expect(failedAddItem.status).toBe(404);
+    })
+
+});
+
+afterAll(() => app.close());
